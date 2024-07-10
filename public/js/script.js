c93a3c333b48449788ba10e1943b968da0708225
archanaprabhat <archanaprabhath2002@gmail.com>
2024-07-10 12:01:55 +0530

    map

--- public/js/script.js
+++ public/js/script.js
@@ -16,4 +16,10 @@ if(navigator.geolocation) {
 );
 }
 
-L.map("map");
+const map = L.map("map").setView([0,0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:"Archana T K"
+}).addTo(map)
+
+const marker ={};
